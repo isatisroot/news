9c088162806dfc78ace6c8267e29ec0c6b391b82
xwp <xwp_fullstack@163.com>
2018-07-19 21:22:33 +0800

    新闻收藏标签的显示

--- info/module/news/view.py
+++ info/module/news/view.py
@@ -11,7 +11,19 @@ def news_detail(news_id):
     news_list_click = News.query.order_by(News.clicks.desc()).limit(CLICK_RANK_MAX_NEWS)
     news = News.query.get(news_id)
 
+    # 判断是否收藏该新闻，默认值为 false
+    is_collection = False
+
+    # 判断用户是否收藏过该新闻
+    if g.user:
+        if news in user.collection_news:
+            is_collection = True
+
+
     return render_template('news/extend.html',
                            news_list_click = news_list_click,
                            news = news,
-                           user =user)
+                           user =user,
+                           is_collection = is_collection)
+
+# 前端根据传入值判断显示哪一个a标签，并使用标签记录当前新闻id，以供后续逻辑使用
